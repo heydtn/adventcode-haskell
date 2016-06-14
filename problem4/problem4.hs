bba576738b5c38a35f1a8a9720149a2c52594386
Nathaniel Heydt <heydtn@gmail.com>
2016-06-14 15:25:57 -0400

    Fixed spacing issue

--- problem4/problem4.hs
+++ problem4/problem4.hs
@@ -10,7 +10,7 @@ main = do
   file <- readFile "problem4.input" >>= return . filter (/= '\n')
   print . head . dropWhile ((/= "00000") . pullPrefix . BS.unpack . hash . BS.pack . (file++) . show) $ [0..]
   where pullPrefix = take 5 . concatMap (padLeft . flip showHex "" . ord)
-        padLeft x =
+        padLeft x  =
           if length x == 1 then
             '0' : x
           else
