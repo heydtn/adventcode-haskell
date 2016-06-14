51bdf9a562229f5839d2307b76c1ef8e049038c0
Nathaniel Heydt <heydtn@gmail.com>
2016-06-14 15:18:38 -0400

    Overhaul of Accumulator object, now splitting santa and robot into seperate move processing and merging later

--- problem3/problem3.hs
+++ problem3/problem3.hs
@@ -2,7 +2,7 @@ import qualified Data.Map as M
 
 type HouseMap = M.Map (Int, Int) Bool
 type Position = (Int, Int)
-type Accumulator = (HouseMap, (Position, Position), Bool)
+type Accumulator = (HouseMap, Position)
 
 movPos :: Position -> Char -> Position
 movPos (x, y) c
@@ -19,22 +19,23 @@ emptyPos :: Position
 emptyPos = (0,0)
 
 emptyAccum :: Accumulator
-emptyAccum = (emptyMap, (emptyPos, emptyPos), False)
-
-insertAndMove2 :: Accumulator -> Char -> Accumulator
-insertAndMove2 (m, (p1,p2), s) c =
-  case s of
-    True  -> (\x -> (M.insert x True m, (p1, x), False)) $ movPos p2 c
-    False -> (\x -> (M.insert x True m, (x, p2), True )) $ movPos p1 c
+emptyAccum = (emptyMap, emptyPos)
 
 insertAndMove  :: Accumulator -> Char -> Accumulator
-insertAndMove (m, (p1,p2), _) c = (\x -> (M.insert x True m, (x, p2), False)) $ movPos p1 c
+insertAndMove (m, p) c = (\x -> (M.insert x True m, x)) $ movPos p c
+
+splitList :: [a] -> ([a], [a])
+splitList []  = ([], [])
+splitList [x] = ([x], [])
+splitList (x:y:zs) = (x:xs, y:ys)
+  where (xs, ys) = splitList zs
 
 main :: IO ()
 main = do
   file <- readFile "problem3.input"
-  let eval  = foldl insertAndMove  emptyAccum file
-  let eval2 = foldl insertAndMove2 emptyAccum file
-  print $ M.size . getFirst $ eval
-  print $ M.size . getFirst $ eval2
-  where getFirst (x, y, z) = x
+  let eval  = processMoves file
+  let evS = processMoves . fst . splitList $ file
+  let evR = processMoves . snd . splitList $ file
+  print . M.size $ eval
+  print . M.size $ M.union evS evR
+  where processMoves = fst . foldl insertAndMove emptyAccum
